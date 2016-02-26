6482a6d153f34f4f8ae52ddb088a6f1b6523e0a6
Simon Nonnis <simononnis@gmail.com>
2016-02-26 13:59:36 +0100

    Update to DB schema query

--- db.js
+++ db.js
@@ -5,28 +5,46 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host : 'localhost',
   user : 'root',
-  password : 'secret',
-  database: 'people'
+  password : 'your_secret',
+  multipleStatements: true
 });
 
-// 2 Create table and insert one record and read it back
+// 2 Establishing explicit DB connection (at point 1 connection was already done)
 connection.connect(function (err) {
-  if (err) throw err;
-  console.log('You are now connected to DB mysql...');
+  if (err)  {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connection to DB mysql established with threadID: ' + connection.threadId);
+});
 
-  connection.query(
-    'CREATE TABLE IF NOT EXISTS `people`.`friends` ( ' +
-    '`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, ' +
-    '`name` VARCHAR(100) NOT NULL, ' +
-    '`occupation` VARCHAR(100) NOT NULL, ' +
-    '`description` TEXT NULL, ' +
-    'PRIMARY KEY (`id`));', function (err) {
+
+// 3 Create table (if not exists) and build schema
+var schema =  'CREATE DATABASE IF NOT EXISTS `people`; ' +
+              'USE `people`; ' +
+              'CREATE TABLE IF NOT EXISTS `people`.`friends` ( ' +
+                '`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, ' +
+                '`name` VARCHAR(100) NOT NULL, ' +
+                '`occupation` VARCHAR(100) NOT NULL, ' +
+                '`description` TEXT NULL, ' +
+                'PRIMARY KEY (`id`) ' +
+              ');';
+
+var createSchema = connection.query(
+  schema
+  , function (err) {
       if (err) throw err;
       console.log('Successfully created schema');
-      connection.end();
-    });
-
 });
+
+
+// 4 Closing DB connection
+connection.end(function (err) {
+  if (err) throw err;
+  console.log('Connection to DB mysql closed');
+});
+
+
 
 // 'CREATE TABLE people(id int(1) primary key NOT NULL AUTO_INCREMENT, name varchar(50), age int, address text)', function (err,  result) {
 //
